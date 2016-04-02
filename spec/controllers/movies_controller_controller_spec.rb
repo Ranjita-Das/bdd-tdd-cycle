646215ddb4e93724037d86b152e6b8ed4e7963a6
ranjita_das <ranjita.renee.das@gmail.com>
2016-04-02 04:05:16 +0000

    last commit

--- spec/controllers/movies_controller_controller_spec.rb
+++ spec/controllers/movies_controller_controller_spec.rb
@@ -3,56 +3,56 @@ require 'spec_helper'
 describe MoviesController do
 
 	before :each do
-		@fake_movie = stub('double').as_null_object
+		@bogus_movie = stub('double').as_null_object
 		@movie = [mock('movie1')]
 	end
 
-	describe 'updating movie info' do
+	describe 'input movie information' do
 		before :each do
-			movie_id = 5
-			Movie.should_receive(:find).with(movie_id.to_s).and_return(@fake_movie)
-			@fake_movie.should_receive(:update_attributes!).exactly 1
+			movie_id = 1
+			Movie.should_receive(:find).with(movie_id.to_s).and_return(@bogus_movie)
+			@bogus_movie.should_receive(:update_attributes!).exactly 1
 			put :update, {:id => movie_id, :movie => @movie}
 		end
-		it 'should call the model method that performs the movie update' do
+		it 'should call the model method for updating movie' do
 			true
 		end
 		it 'should redirect to details template for rendering' do
-			response.should redirect_to(movie_path @fake_movie)
+			response.should redirect_to(movie_path @bogus_movie)
 		end
-		it 'should make updated info available to template' do
-			assigns(:movie).should == @fake_movie
+		it 'should make information that has been updated available to the template' do
+			assigns(:movie).should == @bogus_movie
 		end
 	end
 
-	describe 'finding movies by same director' do
+	describe 'find movies that have the same director' do
 		before :each do
 			@movie_id = 10
-			@founded = [mock('a movie'), mock('another one')]
-			@fake_movie.stub(:director).and_return('fake director')
+			@founded = [mock('a movie'), mock('another')]
+			@bogus_movie.stub(:director).and_return('bogus director')
 		end
 		it 'should render same_director view' do
-			Movie.stub(:find).and_return(@fake_movie)
+			Movie.stub(:find).and_return(@bogus_movie)
 			Movie.stub(:find_all_by_director).and_return(@founded)
 
 			get :same_director, {:id => @movie_id}
 			response.should render_template 'same_director'
 		end
-		it 'should call Model method to get movies with same director' do
-			Movie.should_receive(:find).with(@movie_id.to_s).and_return(@fake_movie)
+		it 'should call Model method to get movies that have the same director' do
+			Movie.should_receive(:find).with(@movie_id.to_s).and_return(@bogus_movie)
 			Movie.should_receive(:find_all_by_director).and_return(@founded)
 
 			get :same_director, {:id => @movie_id}
 		end
-		it 'should make founded movies available to the view' do
-			Movie.stub(:find).and_return(@fake_movie)
+		it 'should make movies that are found available to view' do
+			Movie.stub(:find).and_return(@bogus_movie)
 			Movie.stub(:find_all_by_director).and_return(@founded)
 
 			get :same_director, {:id => @movie_id}
 
 			assigns(:movies).should == @founded
 		end
-		it 'should return to home page, if no movies founded' do
+		it 'should return to home page when no movies are found' do
 			empty_director =  double('movie', :director => '').as_null_object
 			Movie.stub(:find).and_return(empty_director)
 			Movie.stub(:find_all_by_director)
@@ -62,8 +62,8 @@ describe MoviesController do
 		end
 	end
 
-	describe 'create a new movie' do
-		it 'should render the new-movie template' do
+	describe 'creates new movie' do
+		it 'renders new-movie template' do
 			get :new
 			response.should render_template 'new'
 		end
@@ -73,7 +73,7 @@ describe MoviesController do
 
 			post :create, {:movie => movie}
 		end
-		it 'should render home template' do
+		it 'it should render the home template' do
 			movie = stub('new movie').as_null_object
 			Movie.stub(:create!).and_return(movie)
 
@@ -82,13 +82,13 @@ describe MoviesController do
 		end
 	end
 
-	describe 'delete an existing movie' do
-		it 'should render edit movie template' do
+	describe 'deletes a movie that already exists' do
+		it 'should render the edit movie template' do
 			Movie.stub(:find)
-			get :edit, {:id => 5}
+			get :edit, {:id => 1}
 			response.should render_template 'edit'
 		end
-		it 'should call a model method to update data' do
+		it 'should call a model method for updating data' do
 			my_movie = mock('a movie').as_null_object
 			
 			Movie.should_receive(:find).and_return(my_movie)
@@ -96,9 +96,7 @@ describe MoviesController do
 
 			delete :destroy, {:id => 1}
 		end
-		it 'should render show details template' do
-
-		end
+		
 	end
 
 end
